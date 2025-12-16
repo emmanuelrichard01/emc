2eda82badcb29b65102f4bd6139ff6060de723b3
emmanuelrichard01 <emma.moghalu@gmail.com>
2025-12-16 04:19:47 +0100

    chore(postcss): use tailwindcss + autoprefixer in PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 export default {
   plugins: {
+    // Use the official plugin names so Tailwind and PostCSS features (like @apply) work as expected
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
